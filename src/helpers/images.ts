451aacfbcafd44c7871e5aa1a8e744c62fcc3ea7
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-06-01 10:55:12 -0400

    create images

---
+++ src/helpers/images.ts
@@ -0,0 +1,15 @@
+import server$ from 'solid-start/server';
+import { openai } from './config';
+
+// Create n Images by Prompt
+export const createImages = server$(async (prompt, n) => {
+  console.log("Creating Image");
+  const response = await openai.createImage({
+    prompt: prompt,
+    n: n,
+    size: "1024x1024",
+  });
+  console.log(response.data);
+  const urls = response.data.data.map(obj => obj.url);
+  return(urls); // List of Urls
+});
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,41 @@
+import { createImages } from '.././helpers/images';
+import { createSignal } from 'solid-js';
 
 export default function Home() {
+  const [prompt, setPrompt] = createSignal<string>('');
+  const [n, setN] = createSignal<number>(1);
+  const [images, setImages] = createSignal<any[]>([]);
+
+  const handlePromptChange = (event: Event) => {
+    setPrompt((event.target as HTMLInputElement).value);
+  }
+
+  const handleNChange = (event: Event) => {
+    setN(Number((event.target as HTMLInputElement).value));
+  }
+
+  const handleCreateImages = async () => {
+    const res = await createImages(prompt(), n());
+    setImages(res);
+  }
+
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
       <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
         Hello world!
       </h1>
+      <div class="flex flex-col border-2 items-center justify-center">
+        <input class="border-2 m-2" placeholder='Image prompt' value={prompt()} onInput={handlePromptChange} />
+        <input class="border-2" type='number' placeholder='1' value={n()} onInput={handleNChange} />
+        <button onClick={handleCreateImages}>Create Images</button>
+      </div>
+      <div class="flex justify-center align-middle">
+        {images().map((url) => (
+          <div class="w-44 h-44">
+            <img src={url} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
