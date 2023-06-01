300e47bc83e8d577ba7aa136d52d127da032b698
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-06-01 17:23:16 -0400

    wip

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,28 @@
 import { A } from 'solid-start';
+import { createSignal } from 'solid-js';
 
 export default function Home() {
+  const [stories, setStories] = createSignal([
+    {
+      id: 1,
+      title: 'story 1'
+    },
+    {
+      id: 2,
+      title: 'story 2'
+    }
+  ]);
 
   return (
-    <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        Hello world!
-      </h1>
-      <A class="m-3 border-2" href="/story/1">Story</A>
-      <A class="m-3 border-2" href="/story/2">Story</A>
-      <A class="m-3 border-2" href="/story/3">Story</A>
+    <main class="flex flex-col">
+      <h1 class="text-xl m-2 font-semibold">Stories</h1>
+      <div class="flex">
+        {stories().map((story) => (
+          <A class="m-2 p-1 border-2 border-black rounded w-40 h-40" href={`/story/${story.id}`}>
+            <h3>{story.title}</h3>
+          </A>
+        ))}
+      </div>
     </main>
   );
 }
